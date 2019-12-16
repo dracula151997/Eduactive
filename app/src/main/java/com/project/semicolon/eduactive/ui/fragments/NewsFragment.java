b76ec01275e0896c0149174b0c834c94ba65d1c2
HassanMohammed1997 <hassan.s.mohammed1997@gmail.com>
2019-12-16 22:27:08 +0200

    Fix Foreign key bugs in Room database.

--- app/src/main/java/com/project/semicolon/eduactive/ui/fragments/NewsFragment.java
+++ app/src/main/java/com/project/semicolon/eduactive/ui/fragments/NewsFragment.java
@@ -5,22 +5,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.project.semicolon.eduactive.R;
+import com.project.semicolon.eduactive.adapters.News;
+import com.project.semicolon.eduactive.adapters.NewsAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import adapters.News;
-import adapters.NewsAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
