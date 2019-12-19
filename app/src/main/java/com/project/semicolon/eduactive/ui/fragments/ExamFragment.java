2bc11489aefe47e1c15c3b4284a2eea13cc4d0cc
Ahmed-slaah <56643605+Ahmed-slaah@users.noreply.github.com>
2019-12-19 18:14:47 +0200

    Create ExamsFragment

--- app/src/main/java/com/project/semicolon/eduactive/ui/fragments/ExamFragment.java
+++ app/src/main/java/com/project/semicolon/eduactive/ui/fragments/ExamFragment.java
@@ -2,17 +2,15 @@ package com.project.semicolon.eduactive.ui.fragments;
 
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.cardview.widget.CardView;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.project.semicolon.eduactive.ExamAdapter;
 import com.project.semicolon.eduactive.R;
@@ -42,6 +40,10 @@ public class ExamFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
         ExamAdapter adapter = new ExamAdapter();
+        RecyclerView examsRecycler = view.findViewById(R.id.Examrecycler);
+        examsRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
+        examsRecycler.setAdapter(adapter);
+        examsRecycler.setHasFixedSize(true);
 
 
         super.onViewCreated(view, savedInstanceState);
